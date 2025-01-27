93eba51bf64262c7be356bd2821f82cd7615cbe3
Darina Besliney <darinabes92@mail.ru>
2025-01-27 14:25:38 +0300

    внесены правки

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -34,7 +34,11 @@ const App = () => {
     dispatch(resetOrderModal());
   };
 
-  const renderModalRoute = (path: string, title: string, Component: ReactNode) => (
+  const renderModalRoute = (
+    path: string,
+    title: string,
+    Component: ReactNode
+  ) => (
     <Route
       path={path}
       element={
